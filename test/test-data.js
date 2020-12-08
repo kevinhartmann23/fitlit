2fa66c336dd29a8cb15037352655f2d9aff95a38
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-12-08 13:39:00 -0700

    set up sleep test shell, updated test data set, updated arch png

--- test/test-data.js
+++ test/test-data.js
@@ -71,98 +71,186 @@ const activityData = [
       "flightsOfStairs": 32
     }]
 
-    const hydrationData = [
-      {
-        "userID": 1,
-        "date": "2019/06/15",
-        "numOunces": 37
-      },
-      {
-        "userID": 2,
-        "date": "2019/06/15",
-        "numOunces": 75
-      },
-      {
-        "userID": 3,
-        "date": "2019/06/15",
-        "numOunces": 47
-      },
-      {
-        "userID": 4,
-        "date": "2019/06/15",
-        "numOunces": 85
-      },
-      {
-        "userID": 5,
-        "date": "2019/06/15",
-        "numOunces": 42
-      },
-      {
-        "userID": 6,
-        "date": "2019/06/15",
-        "numOunces": 87
-      },
-      {
-        "userID": 7,
-        "date": "2019/06/15",
-        "numOunces": 94
-      },
-      {
-        "userID": 8,
-        "date": "2019/06/15",
-        "numOunces": 84
-      },
-      {
-        "userID": 9,
-        "date": "2019/06/15",
-        "numOunces": 39
-      },
-      {
-        "userID": 10,
-        "date": "2019/06/15",
-        "numOunces": 75
-      }]
+const hydrationData = [
+  {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numOunces": 37
+  },
+  {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numOunces": 75
+  },
+  {
+      "userID": 3,
+      "date": "2019/06/15",
+      "numOunces": 47
+  },
+  {
+      "userID": 4,
+      "date": "2019/06/15",
+      "numOunces": 85
+  },
+  {
+      "userID": 5,
+      "date": "2019/06/15",
+      "numOunces": 42
+  },
+  {
+      "userID": 6,
+      "date": "2019/06/15",
+      "numOunces": 87
+  },
+  {
+      "userID": 7,
+      "date": "2019/06/15",
+      "numOunces": 94
+  },
+  {
+      "userID": 8,
+      "date": "2019/06/15",
+      "numOunces": 84
+  },
+  {
+      "userID": 9,
+      "date": "2019/06/15",
+      "numOunces": 39
+    },
+  {
+      "userID": 10,
+      "date": "2019/06/15",
+      "numOunces": 75
+}]
 
-      const sleepData = [
-        {
-          "userID": 1,
-          "date": "2019/06/15",
-          "hoursSlept": 6.1,
-          "sleepQuality": 2.2
-        },
-        {
-          "userID": 2,
-          "date": "2019/06/15",
-          "hoursSlept": 7,
-          "sleepQuality": 4.7
-        },
-        {
-          "userID": 3,
-          "date": "2019/06/15",
-          "hoursSlept": 10.8,
-          "sleepQuality": 4.7
-        },
-        {
-          "userID": 4,
-          "date": "2019/06/15",
-          "hoursSlept": 5.4,
-          "sleepQuality": 3
-        },
-        {
-          "userID": 5,
-          "date": "2019/06/15",
-          "hoursSlept": 4.1,
-          "sleepQuality": 3.6
-        },
-        {
-          "userID": 6,
-          "date": "2019/06/15",
-          "hoursSlept": 9.6,
-          "sleepQuality": 2.9
-        }]
+const sleepData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7.5,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.7,
+    "sleepQuality": 3.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "hoursSlept": 8,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.9
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "hoursSlept": 10.4,
+    "sleepQuality": 3.1
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/18",
+    "hoursSlept": 10.8,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/18",
+    "hoursSlept": 9.8,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "hoursSlept": 10.7,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/19",
+    "hoursSlept": 9.6,
+    "sleepQuality": 2.5
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/19",
+    "hoursSlept": 7.2,
+    "sleepQuality": 3.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "hoursSlept": 9.3,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/20",
+    "hoursSlept": 10.1,
+    "sleepQuality": 2.4
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/20",
+    "hoursSlept": 9.4,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "hoursSlept": 7.8,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/21",
+    "hoursSlept": 4.3,
+    "sleepQuality": 4.8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/21",
+    "hoursSlept": 8.9,
+    "sleepQuality": 3.7
+  }]
 
-module.exports = {userData}
-  // ,
-  // activityData,
-  // hydrationData,
-  // sleepData
+module.exports = {userData, hydrationData, sleepData }
+
+  // activityData
