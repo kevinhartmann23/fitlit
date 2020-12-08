1a77d3c3442da316b490ecf500c5cf3451e53be3
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-12-07 17:06:34 -0700

    created and passed userjs tests

---
+++ src/user.js
@@ -0,0 +1,23 @@
+
+
+class User {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.address = data.address;
+    this.email = data.email;
+    this.strideLength = data.strideLength;
+    this.dailyStepGoal = data.dailyStepGoal;
+    this.friends = data.friends;
+  }
+
+  getUserFirstName() {
+    let firstName = this.name.split(' ')
+    return firstName[0];
+  }
+
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = User
+}
--- src/userRepo.js
+++ src/userRepo.js
@@ -1,9 +1,10 @@
 class UserRepo {
-  constructor(userData) {
-    this.userData = userData
+  constructor(data) {
+    this.userData = data
   }
 
   getUserData(id) {
+    //change parameter to name, access name - return id
     return this.userData.find(user => user.id === id)
   }
 
@@ -14,6 +15,14 @@ class UserRepo {
     })
     return Math.ceil(totalSteps/this.userData.length)
   }
+
+  // getUserByName(name){
+  //   this.userData.find(user => {
+  //     if(user.name === name){
+  //       return this.getUserData(user.id)
+  //     }
+  //     });
+  // }
 }
 
 if (typeof module !== 'undefined') {
